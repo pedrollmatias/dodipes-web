4e5a63da7c0f1bddaba1963efadaa02a495af586
pedrollmatias <pedrollmatias@gmail.com>
2022-01-16 01:55:38 -0300

    feat: includes add item modal

--- src/components/UI/SlideToggle.jsx
+++ src/components/UI/SlideToggle.jsx
@@ -4,13 +4,13 @@ import classes from "./SlideToggle.module.scss";
 
 const SlideToggle = ({ checked = false, label }) => {
   return (
-    <>
+    <div className="d-flex justify-content-center">
       <label className={classes.switch}>
         <Form.Control type="checkbox" defaultChecked={checked}/>
         <span className={`${classes.slider}`}></span>
       </label>
       {label && <span className="ms-2">{label}</span>}
-    </>
+    </ div>
   );
 };
 
--- src/pages/Menu/Category.jsx
+++ src/pages/Menu/Category.jsx
@@ -1,21 +1,34 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+
 import { FiEdit, FiTrash2 } from "react-icons/fi";
 
-import CategoryItemCard from './CategoryItemCard';
+import CategoryItemCard from "./CategoryItemCard";
 
 import IconButton from "../../components/UI/IconButton";
 import Divider from "../../components/UI/Divider";
+import AddItemModal from "./AddItemModal";
 
 const categoryItems = [
   {
-    name: 'Filé com Fritas',
-    description: '300g de picanha maturada e 500g de fritas',
-    img: 'aaa',
+    name: "Filé com Fritas",
+    description: "300g de picanha maturada e 500g de fritas",
+    img: "aaa",
     active: true,
-    price: 7990
-  }
-]
+    price: 7990,
+  },
+];
 
 const Category = ({ name }) => {
+  const [isEditingItem, setIsEditingItem] = useState(false);
+  const [addItemModalShow, setAddItemModalShow] = useState(false);
+
+  const handleAddItemModalShow = (isInterval = false) => {
+    setAddItemModalShow(true);
+    setIsEditingItem(isInterval);
+  };
+  const handleAddItemModalHide = () => setAddItemModalShow(false);
+
   return (
     <>
       <div className="d-flex align-items-center mb-3">
@@ -30,13 +43,29 @@ const Category = ({ name }) => {
         </div>
       </div>
 
-      {categoryItems.map((categoryItem) => <div key={name} className="pb-2">
-        <CategoryItemCard {...categoryItem} />
-      </div>)}
+      <div className="mb-3">
+        {categoryItems.map((categoryItem) => (
+          <div key={name} className="pb-2">
+            <CategoryItemCard {...categoryItem} />
+          </div>
+        ))}
+      </div>
+
+      <div className="d-flex justify-content-end">
+        <Button
+          className="px-4"
+          variant="primary"
+          onClick={handleAddItemModalShow}
+        >
+          Adicionar item
+        </Button>
+      </div>
 
       <div className="py-3">
         <Divider />
       </div>
+
+      <AddItemModal show={addItemModalShow} onHide={handleAddItemModalHide} />
     </>
   );
 };
